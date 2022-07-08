105c8b3cb2602c570f5175221de71fd7b7dca7fd
misbah335 <89735592+MisbahAlizada@users.noreply.github.com>
2022-07-08 10:56:50 +0430

    first commit

---
+++ SignInController.java
@@ -0,0 +1,47 @@
+package com.example.jfx2.Controllers;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class SignInController {
+
+    private Stage stage;
+    private Scene scene;
+    private Parent root;
+
+
+    @FXML
+    private Button logIn;
+
+    @FXML
+    private Button signIn;
+
+    @FXML
+    private Button signUp;
+
+    @FXML
+    void logIn(ActionEvent event) throws IOException{
+        root = FXMLLoader.load(getClass().getResource("mainPage.fxml"));
+        stage = (Stage) ((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    @FXML
+    void switchToSignUp(ActionEvent event) throws IOException{
+        root = FXMLLoader.load(getClass().getResource("signUp.fxml"));
+        stage = (Stage) ((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+}
